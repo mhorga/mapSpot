b67e2ac4077d379bcb69f8767fdd44c897d80323
Marius Horga <mhorga@gmail.com>
2015-02-13 00:20:12 +0200

    Initial Commit

---
+++ mapSpot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mapSpot
+//
+//  Created by Marius Horga on 2/13/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
